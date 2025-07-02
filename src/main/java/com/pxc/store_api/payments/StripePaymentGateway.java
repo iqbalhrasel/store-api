caa727e4dabe417219a30cf80a4d8949f8103425
Iqbal Hossain <iqbal.h.rasel@gmail.com>
2025-07-02 12:08:32 +0600

    update payment metadata

--- src/main/java/com/pxc/store_api/payments/StripePaymentGateway.java
+++ src/main/java/com/pxc/store_api/payments/StripePaymentGateway.java
@@ -34,7 +34,7 @@ public class StripePaymentGateway implements PaymentGateway{
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntentData(order));
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
@@ -49,6 +49,10 @@ public class StripePaymentGateway implements PaymentGateway{
             throw new PaymentException();
         }
 
+    }
+
+    private SessionCreateParams.PaymentIntentData createPaymentIntentData(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder().putMetadata("order_id", order.getId().toString()).build();
     }
 
     @Override
